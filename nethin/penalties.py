56b115fa22c0a4786400075a5d3597532bcda606
Tommy Löfstedt <tommy.lofstedt@umu.se>
2017-11-28 08:48:36 +0100

    ENH: Fixed bugs in the TotalVariation penalty.

--- nethin/penalties.py
+++ nethin/penalties.py
@@ -10,8 +10,39 @@ Copyright (c) 2017, Tommy Löfstedt. All rights reserved.
 @email:   tommy.lofstedt@umu.se
 @license: BSD 3-clause.
 """
+import abc
+from six import with_metaclass
+
+import keras.backend as K
+
+from nethin.utils import TensorflowHelper
 
 
+class BasePenalty(with_metaclass(abc.ABCMeta, object)):
+    """Base class for penalties.
+
+    Parameters
+    ----------
+    data_format : str, optional
+        One of ``channels_last`` (default) or ``channels_first``. The ordering
+        of the dimensions in the inputs. ``channels_last`` corresponds to
+        inputs with shape ``(batch, height, width, channels)`` while
+        ``channels_first`` corresponds to inputs with shape ``(batch, channels,
+        height, width)``. It defaults to the ``image_data_format`` value found
+        in your Keras config file at ``~/.keras/keras.json``. If you never set
+        it, then it will be "channels_last".
+    """
+    def __init__(self, data_format=None):
+
+        self.data_format = TensorflowHelper.check_data_format(data_format)
+
+    def __call__(self, weights):
+
+        return self.call(weights)
+
+    @abc.abstractmethod
+    def call(self, weights):
+        raise NotImplementedError('"call" has not been specialised.')
 
 
 class TotalVariation2D(BasePenalty):
@@ -19,10 +50,6 @@ class TotalVariation2D(BasePenalty):
 
     Parameters
     ----------
-    name : str
-        The name of the loss. Must be unique. Should only contain alpha-numeric
-        characters.
-
     gamma : float
         Non-negative float. The regularisation constant for the penalty.
 
@@ -31,96 +58,68 @@ class TotalVariation2D(BasePenalty):
         the sum of them. Default is True, compute the mean.
 
     data_format : str, optional
-        The format of the input data. Either "NHWC" for shapes (num_samples,
-        rows, columns, channels) or "NCHW" for shapes (num_samples, channels,
-        rows, columns). Default is "NHWC", i.e. with the channels last.
+        One of ``channels_last`` (default) or ``channels_first``. The ordering
+        of the dimensions in the inputs. ``channels_last`` corresponds to
+        inputs with shape ``(batch, height, width, channels)`` while
+        ``channels_first`` corresponds to inputs with shape ``(batch, channels,
+        height, width)``. It defaults to the ``image_data_format`` value found
+        in your Keras config file at ``~/.keras/keras.json``. If you never set
+        it, then it will be "channels_last".
     """
-    def __init__(self, name, gamma, mean=True, data_format="NHWC"):
+    def __init__(self,
+                 gamma,
+                 mean=True,
+                 data_format=None):
 
-        super(TotalVariation2D, self).__init__(name, trainable=False,
-                                               assignable=False)
+        super(TotalVariation2D, self).__init__(data_format=data_format)
 
         self.gamma = max(0.0, float(gamma))
         self.mean = bool(mean)
-        self.data_format = TensorflowHelper.check_data_format(data_format)
 
-        self._outputs = None
-
-    def generate(self, inputs):
-        """Generates a node in the computational graph for this loss.
+    def call(self, weights):
+        """Computes the total variation of the input.
 
         Parameters
         ----------
-        inputs : tensor or list of tensor, length 1
-            The tensor that connects to this loss.
+        inputs : Tensor
+            The weight matrix for which the total variation should be computed.
+            Should have the shape (B, H, W, C), if
+            ``data_format=channels_last``, and (B, C, H, W) if
+            ``data_format=channels_first``.
         """
-        if not isinstance(inputs, list):
-            inputs = [inputs]
-        if len(inputs) != 1:
-            raise ValueError("Layer accepts only a single input!")
-        inputs = inputs[0]
+        if self.gamma > 0.0:
+            if self.data_format == "channels_last":
+                dif1 = weights[:, 1:, :, :] - weights[:, :-1, :, :]
+                dif2 = weights[:, :, 1:, :] - weights[:, :, :-1, :]
 
-        with tf.name_scope(self.name):
+                edge1 = dif1[:, :, -1:, :]
+                edge2 = dif2[:, -1:, :, :]
 
-            if self.data_format == "NCHW":
-                inputs = tf.transpose(inputs, (0, 2, 3, 1))
-
-#            outputs = tf.reduce_sum(tf.image.total_variation(inputs))
-
-            dif1 = inputs[:, 1:, :, :] - inputs[:, :-1, :, :]
-            dif2 = inputs[:, :, 1:, :] - inputs[:, :, :-1, :]
-
-            edge1 = dif1[:, :, -1:, :]
-            edge2 = dif2[:, -1:, :, :]
-
-            dif1 = dif1[:, :, :-1, :]
-            dif2 = dif2[:, :-1, :, :]
-
-            if self.mean:
-                outputs = tf.reduce_sum(
-                              tf.sqrt(tf.square(dif1) + tf.square(dif2)),
-                              axis=[1, 2, 3]
-                          ) \
-                        + tf.reduce_sum(tf.abs(edge1), axis=[1, 2, 3]) \
-                        + tf.reduce_sum(tf.abs(edge2), axis=[1, 2, 3])
-                outputs = tf.reduce_mean(outputs)
+                dif1 = dif1[:, :, :-1, :]
+                dif2 = dif2[:, :-1, :, :]
             else:
-                outputs = tf.reduce_sum(
-                              tf.sqrt(tf.square(dif1) + tf.square(dif2))
-                          ) \
-                        + tf.reduce_sum(tf.abs(edge1)) \
-                        + tf.reduce_sum(tf.abs(edge2))
+                dif1 = weights[:, :, 1:, :] - weights[:, :, :-1, :]
+                dif2 = weights[:, :, :, 1:] - weights[:, :, :, :-1]
 
-            # TODO: Check if gamma is non-zero before computing anything?
+                edge1 = dif1[:, :, :, -1:]
+                edge2 = dif2[:, :, -1:, :]
+
+                dif1 = dif1[:, :, :, :-1]
+                dif2 = dif2[:, :, :-1, :]
+
+            outputs = K.sum(K.sum(K.sum(K.sqrt(K.square(dif1) + K.square(dif2)),
+                                        axis=1),
+                                  axis=1),
+                            axis=1) \
+                    + K.sum(K.sum(K.sum(K.abs(edge1), axis=1), axis=1), axis=1) \
+                    + K.sum(K.sum(K.sum(K.abs(edge2), axis=1), axis=1), axis=1)
+            if self.mean:
+                outputs = K.mean(outputs)
+            else:
+                outputs = K.sum(outputs)
+
             outputs = self.gamma * outputs
+        else:
+            outputs = self.gamma * weights[0, 0, 0, 0]
 
-            self._outputs = outputs
-
-        return self._outputs
-
-    def get_shape(self, input_shape=None):
-        """Returns the shape of the node output.
-
-        Parameters
-        ----------
-        input_shape : list of tuple of int, optional
-            The shape of the input Tensor to this node.
-
-        Returns
-        -------
-        output_shape : tuple of int
-            The shape of the node's output.
-        """
-        if input_shape is not None:
-            if isinstance(input_shape, list):  # Then a list of a tuple
-                if isinstance(input_shape[0], tuple) \
-                        and (len(input_shape) == 1):
-
-                    input_shape = input_shape[0]
-                else:
-                    raise ValueError("The input_shape is not a list with a "
-                                     "single tuple.")
-
-        output_shape = tuple()  # Scalar tensor output
-
-        return output_shape
+        return outputs

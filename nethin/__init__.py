2957e170d5b6c92b638fdb656c036ffd32a78c7e
Tommy Löfstedt <tommy.lofstedt@umu.se>
2019-03-08 23:19:36 +0100

    MAINT: Added hyper to __ini__.py.

--- nethin/__init__.py
+++ nethin/__init__.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Oct 10 11:02:33 2017
 
-Copyright (c) 2017, Tommy Löfstedt. All rights reserved.
+Copyright (c) 2017-2019, Tommy Löfstedt. All rights reserved.
 
 @author:  Tommy Löfstedt
 @email:   tommy.lofstedt@umu.se
@@ -11,13 +11,22 @@ Copyright (c) 2017, Tommy Löfstedt. All rights reserved.
 from nethin import augmentation
 from nethin import consts
 from nethin import data
+from nethin import hyper
 from nethin import models
 from nethin import normalization
+# from nethin import optimizers
 from nethin import padding
 from nethin import trainers
 from nethin import utils
 
+# models = utils.LazyImport("nethin.models")
+# normalization = utils.LazyImport("nethin.normalization")
+# padding = utils.LazyImport("nethin.padding")
+
 __version__ = "0.0.1"
 
-__all__ = ["augmentation", "consts", "data", "models", "normalization",
-           "padding", "trainers", "utils"]
+__all__ = ["augmentation", "consts", "data", "hyper", "models",
+           "normalization",
+           # "optimizers",
+           "padding", "trainers",
+           "utils"]
